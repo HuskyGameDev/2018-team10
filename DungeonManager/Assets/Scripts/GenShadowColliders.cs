c032e9cca3b1f899d0dba17088dad4076b2ea470
Brandon Johnson <zebrahome10@gmail.com>
2018-10-20 16:37:37 -0400

    Actual fixes to GenShadowColliders.cs

--- DungeonManager/Assets/Scripts/GenShadowColliders.cs
+++ DungeonManager/Assets/Scripts/GenShadowColliders.cs
@@ -59,8 +59,8 @@ public class GenShadowColliders : EditorWindow {
 				if(CanAddShadowCaster(boxCollider)){
 					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 					cube.name = "ShadowCaster";
-					Destroy(cube.GetComponent<BoxCollider2D>());
-					
+					DestroyImmediate(cube.GetComponent<BoxCollider>());
+
 					//Parent object will be the object with the box collider
 					Undo.RegisterCreatedObjectUndo(cube, "Created ShadowCaster");
 					//Undo.SetTransformParent(cube.transform, boxCollider.gameObject.transform, "Set Shadow Caster Parent");
