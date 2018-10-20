235f019b3392a032aa6ea9962a64435093c63fba
Brandon Johnson <zebrahome10@gmail.com>
2018-10-20 16:30:35 -0400

    Changed generated shadow colliders to NOT have box colliders attatched.

--- DungeonManager/Assets/Scripts/GenShadowColliders.cs
+++ DungeonManager/Assets/Scripts/GenShadowColliders.cs
@@ -59,7 +59,8 @@ public class GenShadowColliders : EditorWindow {
 				if(CanAddShadowCaster(boxCollider)){
 					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 					cube.name = "ShadowCaster";
-
+					Destroy(cube.GetComponent<BoxCollider2D>());
+					
 					//Parent object will be the object with the box collider
 					Undo.RegisterCreatedObjectUndo(cube, "Created ShadowCaster");
 					//Undo.SetTransformParent(cube.transform, boxCollider.gameObject.transform, "Set Shadow Caster Parent");
