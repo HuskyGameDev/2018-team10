95f095b50617ad4aa47cd2469eaba64fb555f4d6
unknown <jkiger@mtu.edu>
2018-11-10 17:45:49 -0500

    main menu setup

--- DungeonManager/Assets/Scripts/UI/MainMenu.cs
+++ DungeonManager/Assets/Scripts/UI/MainMenu.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement; 
 
 public class MainMenu : MonoBehaviour {
+	//loads in scenes  
 	public void PlayGame(){
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
+	//Handles quit of the game and sends degub for testing
 	public void QuitGame(){
 		Debug.Log("QUIT!");
 		Application.Quit();
