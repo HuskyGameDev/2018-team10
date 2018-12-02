1283a4741f0354800f13cd862b3e4f4b44d793e0
Brandon Johnson <zebrahome10@gmail.com>
2018-12-01 19:04:32 -0500

    Fix light collider not growing in torches

--- DungeonManager/Assets/Scripts/Lighting/IlluminationManager.cs
+++ DungeonManager/Assets/Scripts/Lighting/IlluminationManager.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class IlluminationManager : MonoBehaviour {
 	public bool illuminated = false;
+	public Torch_Toggle torch = null;
+
+	void FixedUpdate(){
+		if(torch != null){
+			if(torch.isLit){
+				illuminated = true;
+			}else{
+				illuminated = false;
+			}
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if(other.tag == "Torch"){
+		if(other.tag == "Torch" && other.gameObject.GetComponent<Torch_Toggle>().isLit){
 			illuminated = true;
+			torch = other.gameObject.GetComponent<Torch_Toggle>();
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other){
 		if(other.tag == "Torch"){
 			illuminated = false;
+			torch = null;
 		}
 	}
 }
